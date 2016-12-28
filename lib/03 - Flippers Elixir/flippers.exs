8efb440c831fd57aba251b974ae5a230cf119d44
dszopa <szopanator@gmail.com>
2016-12-28 16:02:21 -0600

    Create an elixir implementation of node-flippers * Add documentation

--- lib/03 - Flippers Elixir/flippers.exs
+++ lib/03 - Flippers Elixir/flippers.exs
@@ -1,7 +1,29 @@
 defmodule Flippers do
-  def get_github do
-    IO.puts(HTTPoison.get!("https://api.github.com").status_code)
+  @moduledoc """
+    #Flippers
+
+    This module acts as a wrapper for the rsbuddy grand exchange api
+  """
+
+  # These are called model attributes, they are consistent throughtout the module
+  @rsbuddyapi "api.rsbuddy.com"
+  @rsbuddy "rsbuddy.com"
+
+  @doc """
+    Returns a json string of all items in the current rsbuddy grand exchange database.
+  """
+  def get_all_items() do
+    HTTPoison.get!(@rsbuddy <> "/exchange/summary.json").body
+  end
+
+  @doc """
+  Returns the information about an item from the rsbuddy grand exchange database.
+  The item is selected by its in game id.
+  """
+  def get_item_by_id(id) do
+    HTTPoison.get!(@rsbuddyapi <> "/grandExchange?a=guidePrice&i=" <> id).body
   end
 end
 
-Flippers.get_github
+# You can run this script locally with "mix run lib/03\ -\ Flippers\ Elixir/flippers.exs"
+Flippers.get_all_items
